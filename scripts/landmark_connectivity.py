c7ee6d277b685a6ddb9f8f6d033be4253885c8e5
Jared Velasquez <jaredvel25@gmail.com>
2024-08-29 14:01:57 -0700

    Assigned ownership of landmarks

--- scripts/landmark_connectivity.py
+++ scripts/landmark_connectivity.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 import os
+import random
 import sys
 from typing import Dict, List, Tuple
 from itertools import product
@@ -36,8 +37,37 @@ def append_frequency_to_csv(frequency: Dict[str, List[int]], num_robots: int, cs
                 f.write(f",{count}")
             f.write("\n")
 
+def reindex_landmarks(pyfg_filepath: str, landmark_owner: Dict[str, str], output_dir: str) -> None:
+    """Reindex the landmarks in the PyFG dataset based on the ownership of the landmarks.
 
-def landmark_counter(pyfg_filepath: str, output_dir: str) -> None:
+    Args:
+        pyfg_filepath (str): Filepath to the PyFG dataset
+        landmark_owner (Dict[str, str]): ownership of each landmark
+    
+    Returns:
+        None
+    """
+
+    assert pyfg_filepath.endswith(".pyfg"), logger.critical(
+        "Dataset must be in PyFG format."
+    )
+
+    if (not os.path.isdir(output_dir)):
+        os.makedirs(output_dir)
+
+    # Reindexed PyFG dataset
+    base = os.path.basename(pyfg_filepath)
+    reindexed_pyfg_filepath = os.path.join(output_dir, f"{os.path.splitext(base)[0]}_with_ownership.pyfg")
+    
+    # Replace all instances of the landmark with the new reindexed landmark
+    with open(pyfg_filepath, "r") as f, open(reindexed_pyfg_filepath, "w") as reindexed_f:
+        for line in f:
+            for landmark, owner in landmark_owner.items():
+                line = line.replace(landmark, f"L{owner}{landmark[1:]}")
+            reindexed_f.write(line)
+
+
+def landmark_counter(pyfg_filepath: str, output_dir: str) -> Dict[str, List[int]]:
     """Analyze the connectivity between landmarks and agents in a PyFG dataset.
 
     Args:
@@ -83,6 +113,50 @@ def landmark_counter(pyfg_filepath: str, output_dir: str) -> None:
 
     append_frequency_to_csv(robot_landmark_frequency, pyfg_data.num_robots, frequency_csv_file)
 
+    return robot_landmark_frequency
+
+def assign_ownership_to_landmarks(
+        pyfg_filepath: str, 
+        robot_landmark_frequency: str, 
+        output_dir: str) -> None:
+    """Assign ownership to landmarks to the robot that has the highest frequency of seeing the landmark.
+
+    Args:
+        pyfg_filepath (str): filepath to the PyFG dataset
+        robot_landmark_frequency (Dict[str, List[int]]): frequency of each robot seeing each landmark
+        output_dir (str): directory to save the results with updated ownership
+    
+    Returns:
+        None
+    """
+
+    assert pyfg_filepath.endswith(".pyfg"), logger.critical(
+        "Dataset must be in PyFG format."
+    )
+
+    if (not os.path.isdir(output_dir)):
+        os.makedirs(output_dir)
+
+    logger.info(f"Assigning ownership of landmarks in {pyfg_filepath}")
+    
+    # Assign ownership to landmarks
+    landmark_owner: Dict[str, str] = {}
+    for landmark, freq in robot_landmark_frequency.items():
+        # If none of the robots see the landmark, continue
+        if sum(freq) == 0:
+            continue
+
+        # If tie, randomly assign ownership
+        if all(x == freq[0] for x in freq):
+            robot_idx = math.floor(random.random() * len(freq))
+            landmark_owner[landmark] = get_robot_char_from_number(robot_idx)
+            continue
+
+        max_freq = max(freq)
+        robot_idx = freq.index(max_freq)
+        landmark_owner[landmark] = get_robot_char_from_number(robot_idx)
+
+    reindex_landmarks(pyfg_filepath, landmark_owner, output_dir)
 
 def landmark_connectivity(args) -> None:
     """Recursively scan directory for PyFG files and analyze connectivity between landmarks and agents.
@@ -96,12 +170,16 @@ def landmark_connectivity(args) -> None:
 
     dataset_dir = args.dataset
     output_dir = args.output_dir
+    assign_ownership = args.reindex
 
     for root, dirs, files in os.walk(dataset_dir):
         for file in files:
             if file.endswith(".pyfg"):
                 pyfg_filepath = os.path.join(root, file)
-                landmark_counter(pyfg_filepath, output_dir)
+                frequency = landmark_counter(pyfg_filepath, output_dir)
+
+                if assign_ownership:
+                    assign_ownership_to_landmarks(pyfg_filepath, frequency, output_dir)
 
 def main(args):
     parser = argparse.ArgumentParser(
@@ -112,7 +190,7 @@ def main(args):
         "--dataset",
         type=str,
         required=True,
-        help="Filepath of directory that contains PyFG files to analyze connectivity from",
+        help="filepath of directory that contains PyFG files to analyze connectivity from",
     )
     parser.add_argument(
         "-o",
@@ -120,6 +198,12 @@ def main(args):
         type=str,
         required=True,
         help="directory where results are saved",
+    )
+    parser.add_argument(
+        "-r",
+        "--reindex",
+        action="store_true",
+        help="assign ownership of landmarks to the robot that has the highest frequency of seeing the landmark",
     )
 
     args = parser.parse_args()
